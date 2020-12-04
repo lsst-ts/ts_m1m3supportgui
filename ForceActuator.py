1b298e2739055e232e56101b5e2ada19d0ab1b51
Petr Kubánek <pkubanek@lsst.org>
2020-12-04 15:37:09 +0100

    Common ancestor for ForceActuator page

---
+++ ForceActuator.py
@@ -0,0 +1,155 @@
+# This file is part of M1M3 SS GUI.
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+from PySide2.QtWidgets import (
+    QWidget,
+    QLabel,
+    QPushButton,
+    QHBoxLayout,
+    QVBoxLayout,
+    QFormLayout,
+    QListWidget,
+)
+import QTHelpers
+from TopicData import Topics
+from TimeDeltaLabel import TimeDeltaLabel
+
+
+class ForceActuator(QWidget):
+    """
+    Abstract class for label and graphics display of selected M1M3 values.
+    """
+
+    def __init__(self, comm, userWidget):
+        super().__init__()
+        self.comm = comm
+
+        self.fieldDataIndex = None
+
+        self.layout = QHBoxLayout()
+        self.plotLayout = QVBoxLayout()
+        self.selectionLayout = QVBoxLayout()
+        self.detailsLayout = QFormLayout()
+        self.filterLayout = QHBoxLayout()
+        self.layout.addLayout(self.plotLayout)
+        self.layout.addLayout(self.selectionLayout)
+        self.selectionLayout.addLayout(self.detailsLayout)
+        self.selectionLayout.addWidget(QLabel("Filter Data"))
+        self.selectionLayout.addLayout(self.filterLayout)
+        self.setLayout(self.layout)
+
+        self.selectedActuatorIdLabel = QLabel("")
+        self.selectedActuatorValueLabel = QLabel("")
+        self.selectedActuatorWarningLabel = QLabel("")
+        self.lastUpdatedLabel = TimeDeltaLabel()
+
+        self.topicList = QListWidget()
+        self.topicList.setFixedWidth(256)
+        self.topicList.currentRowChanged.connect(self.currentTopicChanged)
+        self.topics = Topics(comm)
+        for topic in self.topics.topics:
+            self.topicList.addItem(topic.name)
+        self.fieldList = QListWidget()
+        self.fieldList.setFixedWidth(256)
+        self.fieldList.currentRowChanged.connect(self.currentFieldChanged)
+
+        self.plotLayout.addWidget(userWidget)
+
+        self.detailsLayout.addRow(QLabel("Selected Actuator Details"), QLabel(""))
+        self.detailsLayout.addRow(QLabel("Actuator Id"), self.selectedActuatorIdLabel)
+        self.detailsLayout.addRow(
+            QLabel("Actuator Value"), self.selectedActuatorValueLabel
+        )
+        self.detailsLayout.addRow(
+            QLabel("Actuator Warning"), self.selectedActuatorWarningLabel
+        )
+        self.detailsLayout.addRow(QLabel("Last Updated"), self.lastUpdatedLabel)
+
+        self.filterLayout.addWidget(self.topicList)
+        self.topicList.setCurrentRow(0)
+        self.filterLayout.addWidget(self.fieldList)
+
+    def currentTopicChanged(self, topicIndex):
+        if topicIndex < 0:
+            self.setUnknown()
+            return
+
+        self.fieldList.clear()
+        for field in self.topics.topics[topicIndex].fields:
+            self.fieldList.addItem(field[0])
+
+        fieldIndex = self.topics.topics[topicIndex].selectedField
+        if fieldIndex < 0:
+            self.setUnknown()
+            return
+
+        self.fieldList.setCurrentRow(fieldIndex)
+        self.changeField(topicIndex, fieldIndex)
+
+    def currentFieldChanged(self, fieldIndex):
+        topicIndex = self.topicList.currentRow()
+        if topicIndex < 0 or fieldIndex < 0:
+            self.setUnknown()
+            return
+        self.changeField(topicIndex, fieldIndex)
+        self.topics.topics[topicIndex].selectedField = fieldIndex
+
+    def setUnknown(self):
+        self.lastUpdatedLabel.setUnknown()
+
+    def updateSelectedActuator(self, s):
+        if s is None:
+            self.selectedActuatorIdLabel.setText("not selected")
+            self.selectedActuatorValueLabel.setText("")
+            self.selectedActuatorWarningLabel.setText("")
+            return
+
+        self.selectedActuatorIdLabel.setText(str(s.id))
+        self.selectedActuatorValueLabel.setText(str(s.data))
+        QTHelpers.setWarningLabel(self.selectedActuatorWarningLabel, s.warning)
+
+    def changeField(self, topicIndex, fieldIndex):
+        """
+        Redraw actuators with new values.
+        """
+        topic = self.topics.topics[topicIndex]
+        field = topic.fields[fieldIndex]
+        self.fieldGetter = field[1]
+        self.fieldDataIndex = field[2]()
+        try:
+            data = topic.data.get()
+            if data is None:
+                self.setUnknown()
+                return
+
+            self.updateData(data)
+            self.topics.changeTopic(topicIndex, self.dataCallback)
+        except RuntimeError as err:
+            print(err)
+            pass
+
+    def dataCallback(self, data):
+        self.updateData(data)
+        if self.topics.lastCallBack is not None:
+            self.topics.lastCallBack(data)
+
+    def updateData(self, data):
+        self.lastUpdatedLabel.setTime(data.timestamp)
--- ForceActuatorGraphPageWidget.py
+++ ForceActuatorGraphPageWidget.py
@@ -1,144 +1,24 @@
-import QTHelpers
-import copy
 from FATABLE import *
-from TopicData import Topics
-from PySide2.QtWidgets import (
-    QWidget,
-    QLabel,
-    QPushButton,
-    QHBoxLayout,
-    QVBoxLayout,
-    QGridLayout,
-    QListWidget,
-)
+from ForceActuator import ForceActuator
 from ActuatorsDisplay import MirrorWidget, Actuator
-from TimeDeltaLabel import TimeDeltaLabel
 
 
-class ForceActuatorGraphPageWidget(QWidget):
+class ForceActuatorGraphPageWidget(ForceActuator):
     """
     Draw distribution of force actuators, and selected value. Intercept events callbacks to trigger updates.
     """
 
     def __init__(self, comm):
-        super().__init__()
-        self.comm = comm
-
-        self.fieldDataIndex = None
-
-        self.layout = QHBoxLayout()
-        self.plotLayout = QVBoxLayout()
-        self.selectionLayout = QVBoxLayout()
-        self.detailsLayout = QGridLayout()
-        self.filterLayout = QHBoxLayout()
-        self.layout.addLayout(self.plotLayout)
-        self.layout.addLayout(self.selectionLayout)
-        self.selectionLayout.addLayout(self.detailsLayout)
-        self.selectionLayout.addWidget(QLabel("Filter Data"))
-        self.selectionLayout.addLayout(self.filterLayout)
-        self.setLayout(self.layout)
-
-        self.ignoreFieldChange = False
-
-        self.selectedActuatorIdLabel = QLabel("")
-        self.selectedActuatorValueLabel = QLabel("")
-        self.selectedActuatorWarningLabel = QLabel("")
-        self.lastUpdatedLabel = TimeDeltaLabel()
-
-        self.topicList = QListWidget()
-        self.topicList.setFixedWidth(256)
-        self.topicList.currentItemChanged.connect(self.selectedTopicChanged)
-        self.topics = Topics(comm)
-        for topic in self.topics.topics:
-            self.topicList.addItem(topic.name)
-        self.fieldList = QListWidget()
-        self.fieldList.setFixedWidth(256)
-        self.fieldList.currentItemChanged.connect(self.selectedFieldChanged)
-
         self.mirrorWidget = MirrorWidget()
+        super().__init__(comm, self.mirrorWidget)
+
         self.mirrorWidget.mirrorView.selectionChanged.connect(
             self.updateSelectedActuator
         )
-        self.plotLayout.addWidget(self.mirrorWidget)
-
-        row = 0
-        col = 0
-        self.detailsLayout.addWidget(QLabel("Selected Actuator Details"), row, col)
-        row += 1
-        self.detailsLayout.addWidget(QLabel("Actuator Id"), row, col)
-        self.detailsLayout.addWidget(self.selectedActuatorIdLabel, row, col + 1)
-        row += 1
-        self.detailsLayout.addWidget(QLabel("Actuator Value"), row, col)
-        self.detailsLayout.addWidget(self.selectedActuatorValueLabel, row, col + 1)
-        row += 1
-        self.detailsLayout.addWidget(QLabel("Actuator Warning"), row, col)
-        self.detailsLayout.addWidget(self.selectedActuatorWarningLabel, row, col + 1)
-        row += 1
-        self.detailsLayout.addWidget(QLabel("Last Updated"), row, col)
-        self.detailsLayout.addWidget(self.lastUpdatedLabel, row, col + 1)
-
-        self.filterLayout.addWidget(self.topicList)
-        self.topicList.setCurrentRow(0)
-        self.filterLayout.addWidget(self.fieldList)
-
-        self.topics.changeTopic(None, None)
-
-    def selectedTopicChanged(self, current, previous):
-        topicIndex = self.topicList.currentRow()
-        if topicIndex < 0:
-            self.setUnknown()
-            return
-
-        self.fieldList.clear()
-        for field in self.topics.topics[topicIndex].fields:
-            self.fieldList.addItem(field[0])
-
-        fieldIndex = self.topics.topics[topicIndex].selectedField
-        if fieldIndex < 0:
-            self.setUnknown()
-            return
-
-        self.fieldList.setCurrentRow(fieldIndex)
-        self.changePlot(topicIndex, fieldIndex)
-
-    def selectedFieldChanged(self, current, previous):
-        topicIndex = self.topicList.currentRow()
-        fieldIndex = self.fieldList.currentRow()
-        if topicIndex < 0 or fieldIndex < 0:
-            self.setUnknown()
-            return
-        self.changePlot(topicIndex, fieldIndex)
-        self.topics.topics[topicIndex].selectedField = fieldIndex
-
-    def setUnknown(self):
-        self.lastUpdatedLabel.setUnknown()
-
-    def changePlot(self, topicIndex, fieldIndex):
-        """
-        Redraw actuators with values.
-        """
-        topic = self.topics.topics[topicIndex]
-        field = topic.fields[fieldIndex]
-        self.fieldGetter = field[1]
-        self.fieldDataIndex = field[2]()
-        try:
-            data = topic.data.get()
-            if data is None:
-                self.setUnknown()
-                return
-
-            self.updateData(data)
-            self.topics.changeTopic(topicIndex, self.dataCallback)
-        except RuntimeError as err:
-            print(err)
-            pass
-
-    def dataCallback(self, data):
-        self.updateData(data)
-        if self.topics.lastCallBack is not None:
-            self.topics.lastCallBack(data)
 
     def updateData(self, data):
+        super().updateData(data)
+
         warningData = self.comm.MTM1M3.evt_forceActuatorWarning.get()
         points = []
         values = self.fieldGetter(data)
@@ -178,15 +58,3 @@ class ForceActuatorGraphPageWidget(QWidget):
             self.selectedActuatorValueLabel.setText(
                 str(values[self.mirrorWidget.mirrorView.selected.dataIndex])
             )
-        self.lastUpdatedLabel.setTime(data.timestamp)
-
-    def updateSelectedActuator(self, s):
-        if s is None:
-            self.selectedActuatorIdLabel.setText("not selected")
-            self.selectedActuatorValueLabel.setText("")
-            self.selectedActuatorWarningLabel.setText("")
-            return
-
-        self.selectedActuatorIdLabel.setText(str(s.id))
-        self.selectedActuatorValueLabel.setText(str(s.data))
-        QTHelpers.setWarningLabel(self.selectedActuatorWarningLabel, s.warning)
