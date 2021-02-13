f5fa5634ba157c54b4006fe2d23d08e5c4c517cd
Petr Kubánek <pkubanek@lsst.org>
2021-02-13 19:51:25 -0300

    fixed units conversion

--- UnitsConversions.py
+++ UnitsConversions.py
@@ -2,8 +2,8 @@ import astropy.units as u
 
 __all__ = ["M2MM", "R2ARCSEC", "MM2M", "ARCSEC2R"]
 
-M2MM = u.m.to(u.mm).value
-R2ARCSEC = u.rad.to(u.arcsec).value
+M2MM = (1 * u.m).to(u.mm).value
+R2ARCSEC = (1 * u.rad).to(u.arcsec).value
 
 MM2M = 1 / M2MM
 ARCSEC2R = 1 / R2ARCSEC
